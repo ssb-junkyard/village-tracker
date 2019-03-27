92f305e620be37684bfbf00ff0a0270e9d132994
Dominic Tarr <dominic.tarr@gmail.com>
2019-03-28 03:03:58 +1300

    deterministically randomize (based on week number) the order of modules, issues, and people so that everyone has the same chance of getting something

--- assign-tasks.js
+++ assign-tasks.js
@@ -1,6 +1,11 @@
 var template = require('./template')
 var opts = require('minimist')(process.argv.slice(2))
 var cont = require('cont')
+var Mentions = require('ssb-mentions')
+
+if(!Number.isInteger(opts.week)) throw new Error('--week should be a number')
+var MT = new (require('rng').MT)(opts.week)
+var random = MT.random.bind(MT)
 
 function fromCSV (data) {
   if(!data) return undefined
@@ -13,20 +18,32 @@ function fromCSV (data) {
   return obj
 }
 
+function shuffel (ary) {
+  if(!ary) return ary
+  return ary.sort(function () {
+    return random() - 0.5
+  })
+}
+
 function fromList (s) {
   if(!s) return undefined
   return s.split(/\s+/)
 }
 
-var names = fromCSV(opts.names)
+var names_ordered = fromCSV(opts.names)
+var names = {}
+shuffel(Object.keys(names_ordered)).forEach(function (name) {
+  names[name] = names_ordered[name]
+})
+
 
 var num = Object.keys(names).length
 
-var issues = fromList(opts.issues)
-var modules = fromList(opts.modules)
+var issues = shuffel(fromList(opts.issues))
+var modules = shuffel(fromList(opts.modules))
 
 function getPer (list, num) {
-  return Math.floor((list && list.length || 0) / num)
+  return Math.floor((list && list.length || 0) / num) || 1
 }
 
 function getNext(list, per) {
@@ -40,8 +57,7 @@ function getRest (list, per) {
 
 var issuesPer = getPer(issues, num)
 var modulesPer = getPer(modules, num)
-console.error(issuesPer, modulesPer)
-
+console.log('issues per:', issuesPer, 'modules pes:', modulesPer)
 var VillageTrackerBot = '@KYq92uiEKPcKZIBJ/ZCGYErdcy8n0RYD/nDUtKmuyZU=.ed25519'
 
 require('ssb-client')(function (err, sbot) {
@@ -52,23 +68,25 @@ require('ssb-client')(function (err, sbot) {
     var id = names[name]
     var _issues
     var _issues = getNext(issues, issuesPer)
-  //  issues = getRest(issues, issuesPer)
     var _modules = getNext(modules, modulesPer)
-  //  modules = getRest(modules, modulesPer)
+
 
     var data = {
       name, id, issues: _issues, modules: _modules, week: opts.week, checklist: opts.checklist
     }
+
+    var text = template(data)
     var content = {
       type: 'post',
-      text: template(data),
-      mentions: [{link: id, name}],
+      text: text,
+      mentions: Mentions(text),
       channel: 'village-tracker'
     }
 
     msgs.push(content)
   }
 
+  if(opts.dry) return console.log(JSON.stringify(msgs, null, 2))
   cont.series(msgs.map(function (content) {
     return function (cb) {
       sbot.identities.publishAs({
@@ -81,5 +99,12 @@ require('ssb-client')(function (err, sbot) {
   })
 
 })
+
+
+
+
+
+
+
 
 
